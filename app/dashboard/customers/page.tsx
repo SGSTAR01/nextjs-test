01f44eb5925a303c27c1931e5751ac40a0039497
SGSTAR01 <startroidx@gmaill.com>
2024-08-23 21:31:47 +0530

    feat: Add dashboard pages and layout components

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,10 @@
+import { Metadata } from "next"
+
+export const metadata: Metadata = {
+    title: 'Customers',
+}
+
+// eslint-disable-next-line import/no-anonymous-default-export, react/display-name
 export default function(){
     return(
         <p>Customers Page</p>
